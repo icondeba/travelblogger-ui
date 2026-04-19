2308e139f0fdf431a3505c7fc4aaff2339ed8309
Pronay <nathpronay@gmail.com>
2026-04-20 02:06:08 +0530

    fix: point main UI apiBaseUrl directly to Function App URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://travelblogger-service-ebdnd3aahbfuc7hx.centralindia-01.azurewebsites.net',
   contactEndpoint: '/api/contact-messages',
   videosEndpoint: '/api/videos'
 };
